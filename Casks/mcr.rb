8c7729af71d55b237f53f5019bc1bd6baa7c114d
Mayckol Rezende <43302737+mayckol@users.noreply.github.com>
2026-07-03 18:42:54 +0100

    mcr 0.3.0

--- Casks/mcr.rb
+++ Casks/mcr.rb
@@ -1,8 +1,8 @@
 cask "mcr" do
-  version "0.1.0"
-  sha256 "9c54ce05882f0c32597d8d1e6084e820c7729fd7ad7e77675a8a3ba9514a4e20"
+  version "0.3.0"
+  sha256 "5d33564b898c535e2d197930f012eeea81a7fc965fda5c8a0f091b4cd58b5cee"
 
-  url "https://github.com/mayckol/mcr/releases/download/v#{version}/MCR_0.1.0_aarch64.dmg"
+  url "https://github.com/mayckol/mcr/releases/download/v#{version}/MCR_0.3.0_aarch64.dmg"
   name "MCR"
   desc "Three-pane visual git merge editor"
   homepage "https://github.com/mayckol/mcr"
@@ -12,6 +12,8 @@ cask "mcr" do
 
   app "MCR.app"
 
+  # Unsigned/ad-hoc build: strip the download quarantine on install so
+  # the app opens on a double-click instead of Gatekeeper blocking it.
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-dr", "com.apple.quarantine", "#{appdir}/MCR.app"]
