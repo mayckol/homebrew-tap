6c7319e36e4414fadc6e74fb137982ac929f9bc1
Mayckol Rezende <43302737+mayckol@users.noreply.github.com>
2026-07-08 16:37:14 +0000

    mcr 0.3.11

--- Casks/mcr.rb
+++ Casks/mcr.rb
@@ -1,8 +1,8 @@
 cask "mcr" do
-  version "0.3.10"
-  sha256 "dbd95257937b8aced63f7d97573ff17f06d33b9805ecb9aad481c8285c9d4f52"
+  version "0.3.11"
+  sha256 "fb0d7eb7441d604779eb71ba09a3491c45a72fd5e09affacf45fb4048b86d91f"
 
-  url "https://github.com/mayckol/mcr/releases/download/v#{version}/MCR_0.3.10_aarch64.dmg"
+  url "https://github.com/mayckol/mcr/releases/download/v#{version}/MCR_0.3.11_aarch64.dmg"
   name "MCR"
   desc "Three-pane visual git merge editor"
   homepage "https://github.com/mayckol/mcr"
